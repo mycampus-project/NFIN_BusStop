070cf9b6236e4d63f4e14022668fc14b78e46000
KulaginArtur <artur.kulagin@gmail.com>
2021-03-04 20:28:23 +0200

    Adding campuses

---
+++ src/Campuses.js
@@ -0,0 +1,17 @@
+const campuses = [
+  {
+    id: 1,
+    name: 'Karaportti',
+    lat: 34.43,
+    long: 34.56
+  },
+  {
+    id: 2,
+    name: 'Myyrmäki',
+    lat: 64.34,
+    long: 45.756
+  },
+];
+
+module.exports = campuses;
+

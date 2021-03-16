35fa57b36d55d544c28b5fe8945b86714e14c62d
KulaginArtur <artur.kulagin@gmail.com>
2021-03-16 11:17:27 +0200

    Adding the fetch function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import Header from './components/Header'
+import { graphql } from 'graphql';
 
 function App() {
+
+  useEffect(() => {
+    const url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
+    fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',  {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ "query": `{
+        stop(id: \"HSL:1040129\") {
+          name
+          lat
+          lon
+          wheelchairBoarding
+        }
+      }` }),
+    })
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    /*
+    const data = response.json();
+    console.log(data)*/
+  })
+
   return (
-    <div className="App">
-     <Header color='blue' title='Public Transport' />
-    </div>
+      <div className="App">
+        <Header color='blue' title='Public Transport' />
+      </div>
   );
 }
+
 
 export default App;

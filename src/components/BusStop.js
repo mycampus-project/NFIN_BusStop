760eaec5d63a248ab9967554f8c69e82990333c1
wilhelmo360 <wilhelmo313@gmail.com>
2021-03-19 09:21:16 +0200

    Update BusStop.js

--- src/components/BusStop.js
+++ src/components/BusStop.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import Box from "@material-ui/core/Box";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,6 +15,10 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     maxWidth: 500,
     backgroundColor: "green",
+    borderRadius: "10",
+    borderColor: "primary.main",
+    border: 1,
+    borderWidth: "4px solid",
   },
 }));
 
@@ -23,20 +28,54 @@ export default function BusStop() {
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
+        <Grid container spacing={1}>
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={6} container direction="column" spacing={2}>
               <Grid item xs>
                 <Typography gutterBottom variant="h4" align="left">
                   Pysäkki
                 </Typography>
-                <Typography gutterBottom variant="body1" align="left">
-                  Linja1
-                </Typography>
-                <Typography gutterBottom variant="body1" align="left">
-                  Linja2
-                </Typography>
+                <Grid item xs
+                  container spacing={3}
+                  direction="row"
+                  justify="space-evenly"
+                  alignItems="stretch" >
+                  <Grid>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Linja1
+                    </Typography>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Linja2
+                    </Typography>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Linja3
+                    </Typography>
+                  </Grid>
+                  <Grid>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Aika1
+                    </Typography>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Aika2
+                    </Typography>
+                    <Typography gutterBottom variant="body1" align="left">
+                      Aika3
+                    </Typography>
+                  </Grid>
+                </Grid>
+                
               </Grid>
+            </Grid>
+            <Grid item xs={6} direction="column">
+              <Paper backgroundColor="white">
+                <Typography>TEST</Typography>
+              </Paper>
             </Grid>
           </Grid>
         </Grid>

86ee6920ec2049f103a5f42fcce334137a226ea3
KulaginArtur <artur.kulagin@gmail.com>
2021-03-26 16:27:36 +0200

    Fetches every min

--- src/components/Query.js
+++ src/components/Query.js
@@ -9,47 +9,51 @@ const Query = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-
-      const query = {"query": `{
-        stopsByRadius(lat:${campuses[0].lat},lon:${campuses[0].long},radius:500,first:4) {
-          edges {
-            node {
-              stop { 
-                gtfsId 
-                name
-                stoptimesWithoutPatterns {
-                  realtimeArrival
-                  serviceDay
-                  headsign
-                  trip{
-                    routeShortName
-                  }
-                }
+  const query = {"query": `{
+    stopsByRadius(lat:${campuses[0].lat},lon:${campuses[0].long},radius:500,first:4) {
+      edges {
+        node {
+          stop { 
+            gtfsId 
+            name
+            stoptimesWithoutPatterns {
+              realtimeArrival
+              headsign
+              trip{
+                routeShortName
               }
-              distance
             }
           }
+          distance
         }
-      }`
       }
+    }
+  }`
+  }
 
-    fetch(url, {
-      method: 'POST',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(query),
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setData(data.data)
-      setLoading(false)
-    })
-    .catch(err => {
-      console.error("Error fetching data:", err)
+  useEffect(() => {
 
-    })
-  },[url])
+    const fetchData = () => {
+      fetch(url, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(query),
+      })
+      .then(res => res.json())
+      .then(data => {
+        setData(data.data)
+        setLoading(false)
+      })
+      .catch(err => {
+        setError(err)
+        console.error(error)
+      })
+    }
+
+    setInterval(() => {
+      fetchData()
+   }, 1000 * 60)
+  }, [])
 
   return (
     <div>

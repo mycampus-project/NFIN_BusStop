e411f68c1a59084ae67fd83ba729b381f0ee8bda
wilhelmo360 <wilhelmo313@gmail.com>
2021-03-29 12:16:10 +0300

    Navigation working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Header from './components/Header'
-import Query from './components/Query'
-import BaseGrid from './components/Basegrid'
+
+import NavTabs from './components/Tabs'
+
 
 //this is redux comment
 
 function App() {
   return (
       <div className="App">
-        <Header color='#183693' title='Public Transport' />
-        <Query/>
+
+        <NavTabs/>
+
       </div>
   );
 }
--- src/components/Basegrid.js
+++ src/components/Basegrid.js
@@ -2,15 +2,17 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import BusStop from "./BusStop"
+import { Paper } from "@material-ui/core";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(3),
     textAlign: "center",
-    color: theme.palette.text.secondary,
+    color: "white",
   },
 }));
 
@@ -22,21 +24,26 @@ export default function BaseGrid(props) {
   }
   return (
     <div className={classes.root}>
+      <Paper>
       <Grid
         container
         direction="column"
         justify="flex-start"
         alignItems="stretch"
-        spacing={3}>
+        spacing={1}
+        margin={1}>
 
         {props.data.stopsByRadius.edges.map((data) => {
           return (
           <Grid item >
+            <Paper elevation={3}>
             <BusStop data={data}/>
+            </Paper>
           </Grid>
           )})}
         
       </Grid>
+      </Paper>
     </div>
   )
 }
